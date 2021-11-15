86b8e6225892ecf22fd2128e7caf36dac8e1527b
JannisMueller <jannis.mueller@hotmail.de>
2021-11-15 08:34:10 +0100

    fixes entity

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,12 +1,31 @@
 package se.iths.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
+@Entity
 public class Student {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2)
     private String firstName;
+    @NotEmpty
+    @Size(min = 2)
     private String lastName;
+    @NotEmpty
+    @Email(regexp = "@*")
     private String email;
+
+    @Digits(fraction = 0, integer = 10)
     private String phoneNumber;
 
 }
