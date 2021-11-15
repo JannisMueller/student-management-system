d339e73db56896f3e9169fc528bd401aa24c22fa
JannisMueller <jannis.mueller@hotmail.de>
2021-11-15 08:40:15 +0100

    WIP

--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,23 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.ws.rs.core.Response;
+
+@Transactional
 public class StudentService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Response createNewStudent(Student student){
+        entityManager.persist(student);
+
+        return Response.ok(student).build();
+    }
 
 
 }
